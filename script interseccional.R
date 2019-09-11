269d5aa021aec672ee3e08d73add042aadfddc15
sientifiko <socialscienceanime@gmail.com>
2019-09-11 12:40:18 -0300

    fin del comunidado

--- script interseccional.R
+++ script interseccional.R
@@ -1,5 +1,8 @@
 library(ggplot2); library(tidyverse); library(stringr); library(stargazer)
 
+
+# CAMBIAR TODO ESTO A RELACIÓN ONVRE VS INDÍGENA!!!!!!
+
 # desactivamos la notación científica, y redondeamos a dos
 options(scipen = 999, digits = 2)
 
@@ -46,51 +49,89 @@ quintil_df <- casen_mayores2 %>%
 ggplot(quintil_df, aes(x=sexo2, y = perc, fill = romano) ) +
   geom_bar(stat = "identity", position = "fill") +
   geom_text(aes(label= scales::percent(perc)), 
-            position = position_stack(vjust = .5) )+
+            position = position_stack(vjust = .5))+
   theme_bw() +
   theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   labs(x="", y= "Proporción", fill= "Quintil autónomo\nnacional")
 
 
-# plot multi xd
-ggplot(na.omit(casen_mayores), aes(sexo2, fill=as.factor(multidimensional_5)))+
-  geom_bar(position = "fill")
+# construyo una tabla para plotear la pobreza multidimensional
+multidimensional <- casen_mayores2 %>%
+  group_by(sexo2, multidimensional_5) %>%
+  summarize(n= n()) %>%
+  mutate(perc = n/sum(n))
+  
+# ploteo multidimensional
+ggplot(multidimensional, aes(sexo2, y= perc, fill = as.factor(multidimensional_5) )) +
+  geom_bar(stat = "identity", position = "fill") +
+  geom_text(aes(label=scales::percent(perc)),
+            position = position_stack(vjust = .5)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  labs(x="", y= "Proporción", fill= "Pobreza\nmultidimensional")
 
-ggplot(na.omit(casen_mayores), aes(sexo, fill=as.factor(multidimensional_5)))+
-  geom_bar(position = "fill")
+# cambiar orden de es indigena
 
-ggplot(na.omit(casen_mayores), aes(es_indigena, fill=as.factor(multidimensional_5)))+
-  geom_bar(position = "fill")
+casen_mayores2$es_indigena <- relevel(casen_mayores2$es_indigena, "No indigena")
+
+# Hay intereacción??
+ggplot(casen_mayores2, aes(es_indigena, multidimensional_5 , color=sexo, group=sexo)) +
+  geom_point(stat = "summary", fun.y= mean) +
+  stat_summary(fun.y=mean, geom = "line") +
+  stat_summary(fun.data = mean_se, geom ="pointrange" ) +
+  theme_bw() +
+  theme(legend.position = "top") +
+  labs(x="", y="Promedio pobreza multidimensional")
+
 
 
 # paparando el set específico para los test
-df_trabajo <- casen_mayores[,c(2,6, 7, 14, 16)]
+df_trabajo <- casen_mayores2[,c(2, 6, 7, 14)]
+
+# dar vuelta el índice de sexo
+df_trabajo$sexo_cod2 <- (df_trabajo$sexo_cod-1)^2
 
 
-# ajustando modelos
-
-logit.model1 <- glm(multidimensional_5 ~ sexo_cod, data = df_trabajo, family = binomial())
-
-logit.model2 <- glm(multidimensional_5 ~ sexo_cod*es_indigena_cod,
-                   data = df_trabajo, family = binomial())
-
-logit.model3 <- glm(multidimensional_5 ~ sexo_cod*es_indigena_cod*disc_identidad_genero,
+# ajustando modelo de onvre*indigena
+logit.model1 <- glm(multidimensional_5 ~ sexo_cod2*es_indigena_cod, 
                     data = df_trabajo, family = binomial())
 
-
-stargazer(logit.model3, type = "text")
-
-# mostrando regresion
-
-df_trabajo %>% na.omit() %>%
-  mutate(pred_logit = predict(logit.model3)) -> df
-
-ggplot(df, aes(as.factor(sexo_cod), pred_logit, 
-               color=as.factor(es_indigena_cod) )) +
-  geom_jitter()
+# mostrando modelo doble interacción
+stargazer(logit.model1, type = "text")
 
 
-ggplot(df, aes(pred_logit, fill=as.factor(sexo_cod))) + 
-  geom_density()
+
+# construyo un factor de discriminación por género, ya que ésta está en 0 y 1
+casen_mayores2$es_discriminado <- factor(casen_mayores2$disc_identidad_genero, levels = c(0,1),
+                                         labels = c("No discriminado", "Discriminado"))
+
+# graficando interacicion onvre*discriminacion_genero
+ggplot(casen_mayores2, aes(es_discriminado, multidimensional_5 , color=sexo, group=sexo)) +
+  geom_point(stat = "summary", fun.y= mean) +
+  stat_summary(fun.y=mean, geom = "line") +
+  stat_summary(fun.data = mean_se, geom ="pointrange" ) +
+  theme_bw() +
+  theme(legend.position = "top") +
+  labs(x="", y="Promedio pobreza multidimensional",
+       title = "Discriminación por identidad de género") 
 
 
+# ajustando modelo de onvre*discriminacion_genero
+logit.model2 <- glm(multidimensional_5 ~ sexo_cod2*disc_identidad_genero,
+                    data = df_trabajo, family = binomial())
+
+#mostrando resultado modelo triple interaccion
+stargazer(logit.model1, logit.model2, type = "text")
+
+# Comparación de modelos por Criterio de información de Aikake y Bayes
+AIC(logit.model1, logit.model2)
+BIC(logit.model1, logit.model2)
+
+
+# Ajustando un modelo de solo dos predictores
+logit.model <- glm(multidimensional_5 ~ sexo_cod2+es_indigena_cod, 
+                   data = df_trabajo, family = binomial())
+
+# Comparando si modelos de interacción son mejores que el sin interacción
+AIC(logit.model,logit.model1, logit.model2)
+BIC(logit.model,logit.model1, logit.model2)
